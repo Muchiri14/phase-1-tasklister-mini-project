fd295902bae7085e0385df8aedb185c0da56c89a
Daniel <muchiridaniel59@gmail.com>
2022-12-06 07:20:13 -0500

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    form.reset()
+  })
 });
+function toDoList (list){
+  let p= document.createElement('p')
+  let btn= document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent='x'
+  p.textContent = `${list}`
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#list_container').appendChild(p)
+}
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
